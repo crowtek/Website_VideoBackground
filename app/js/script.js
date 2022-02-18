83a21f33e412c2135234f54e5fe8c73649760b14
crowtek <prt3@hotmail.de>
2022-02-18 16:54:56 +0100

    visual update

--- app/js/script.js
+++ app/js/script.js
@@ -2,7 +2,7 @@
 var video = document.getElementById("myVideo");
 
 // Get the button
-var btn = document.getElementById("playButton");
+var btn = document.getElementById("bubbly-button");
 
 // Pause and play the video, and change the button text
 function vidFunction() {
@@ -14,3 +14,22 @@ function vidFunction() {
     btn.innerHTML = "Play";
   }
 }
+
+
+var animateButton = function(e) {
+
+	e.preventDefault;
+	//reset animation
+	e.target.classList.remove('animate');
+	
+	e.target.classList.add('animate');
+	setTimeout(function(){
+	  e.target.classList.remove('animate');
+	},700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+	bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
